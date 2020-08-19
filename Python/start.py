d3cb5abacd5701ea9735a2a194d760c74f61d758
Volen Nakov <volen.nakov@gmail.com>
2020-08-20 01:46:01 +0300

    first commit

---
+++ Python/start.py
@@ -0,0 +1,23 @@
+import urllib.request
+import os
+import os.path
+from os import path
+
+if path.exists("./server.jar"):
+    os.system("java -Xms8G -Xmx8G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar nogui")
+else:
+    print('Downloading latest Paper spigot...')
+    url = 'https://papermc.io/api/v1/paper/1.14.4/latest/download'
+    urllib.request.urlretrieve(url, './server.jar')
+    
+    eula = "./eula.txt"
+    eula = open(eula, "w")
+    eula.write("eula=true")
+    eula.close()
+
+    server_properties = "./server.properties"
+    server_properties = open(server_properties, "w")
+    server_properties.write("online-mode=false")
+    server_properties.close()
+    
+    os.system("java -Xms8G -Xmx8G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar nogui")
